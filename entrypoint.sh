6996324118f94c6d3109190e9235057f2363d6c6
massonetto <massonetto@bentivis.com>
2020-09-21 18:49:21 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,7 +57,7 @@ find_files () {
 		MAXDEPTH_KEY=""
 	fi
 
-	find $in_dir ${MAXDEPTH_KEY} ${MAXDEPTH_VAL} -type f -name "*.$1" | grep -v ".min.$1$"
+	find $in_dir ${MAXDEPTH_KEY} ${MAXDEPTH_VAL} -type f ! -name "*no-minify*" -name "*.$1" | grep -v ".min.$1$"
 }
 
 exec_minify_js () {
